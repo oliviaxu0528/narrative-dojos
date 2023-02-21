af35d4eea7fd2211014b17cc1636be1599a80d8c
Olivia_Xu <oliviaxu0528@Gmail.com>
2023-02-20 20:53:58 -0800

    book-api all work

--- books/queries/books.py
+++ books/queries/books.py
@@ -1,31 +1,140 @@
 from pydantic import BaseModel
-# from typing import Optional, List, Union
+from typing import Optional, List, Union
 from datetime import date
 from queries.pool import pool
+
+class Error(BaseModel):
+    message: str
 
 class BookIn(BaseModel):
     title:str
     author:str
     image_url:str
-    create_on: date
-class
+    created_on: date
+
+class BookOut(BaseModel):
+    id: int
+    title:str
+    author:str
+    image_url:str
+    created_on: date
+
 class BookRepository(BaseModel):
+    def get_one(self, book_id:int) -> Optional[BookOut]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    result = cur.execute(
+                    """
+                    SELECT id,title,author,image_url,created_on
+                    FROM books
+                    where id = %s
+                    """,
+                    [book_id]
+                    )
+                    record = result.fetchone()
+                    if record is None:
+                        return None
+                    return self.record_to_book_out(record)
+        except Exception as e:
+            print(e)
+            return False
+
+    def delete(self, book_id:int)-> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                    """
+                    Delete from books
+                    where id = %s
+                    """,
+                    [book_id]
+                    )
+                    return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def update(self, book_id:int, book:BookIn) -> Union[BookOut,Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        UPDATE books
+                        SET title = %s,
+                            author = %s,
+                            image_url = %s,
+                            created_on = %s
+                        WHERE id = %s
+                        """,
+                        [
+                            book.title,
+                            book.author,
+                            book.image_url,
+                            book.created_on,
+                            book_id
+                        ]
+                    )
+                    return self.book_in_to_out(book_id,book)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not update"}
+
+    def get_all(self) -> Union[Error, List[BookOut]]:
+            try:
+                with pool.connection() as conn:
+                    with conn.cursor() as cur:
+                        result = cur.execute(
+                            """
+                            SELECT id,title,author,image_url,created_on
+                            FROM books
+                            ORDER BY title;
+                            """
+                        )
+
+                        return [ self.record_to_book_out(record)
+                                for record in result
+                            ]
+            except Exception as e:
+                print(e)
+                return {"message": "Could not get all books"}
+
     def create(self,book:BookIn)-> BookOut:
-        with pool.connection() as connnection:
-            with conn.cursor() as cur:
-                result = cur.execute(
-                """
-                insert into books
-                    (title author image_url created_on)
-                values
-                    (%s,%s,%s,%s);
-                returning id;
-                """,
-                [
-                    book.title,
-                    book.author,
-                    book.image_url,
-                    book.created_on
-                ]
-            )
-                print(result)
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    result = cur.execute(
+                    """
+                    INSERT INTO books
+                        (title, author, image_url, created_on)
+                    VALUES
+                        (%s, %s, %s, %s)
+                    RETURNING id;
+                    """,
+
+                    [
+                        book.title,
+                        book.author,
+                        book.image_url,
+                        book.created_ona
+                    ]
+                )
+                    id = result.fetchone()[0]
+                    return self.book_in_to_out(id,book)
+        except Exception:
+            return {"message":"Could not create"}
+
+    def book_in_to_out(self, id:int, book:BookIn):
+        old_data = book.dict()
+        return BookOut(id=id,**old_data)
+
+    def record_to_book_out(self, record):
+        return BookOut(
+            id=record[0],
+            title=record[1],
+            author=record[2],
+            image_url=record[3],
+            created_on=record[4],
+        )
--- books/routers/books.py
+++ books/routers/books.py
@@ -1,23 +1,49 @@
 # from fastapi import APIRouter, Depends, Response
-# from typing import Union, List,Optional
-from queries.books import BookIn, BookRepository
-# @router.post("/books")
+from typing import Union, List,Optional
+from queries.books import BookIn, BookRepository, BookOut, Error
 # @router.get("/books")
-# @router.put("/books/{book_id}")
 # @router.delete("/books/{book_id}")
 # @router.get("/books/{book_id}")
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 
 router= APIRouter()
 
-@router.post("/books")
+@router.post("/books", response_model = Union[BookOut,Error])
 def create_book(
     book:BookIn,
     repo:BookRepository = Depends()
     ):
-    print(repo)
+    return repo.create(book)
+
+@router.get("/books",response_model=Union[list[BookOut],Error])
+def get_books(
+    repo: BookRepository = Depends()
+):
+    return repo.get_all()
+
+@router.put("/books/{book_id}",response_model=Union[BookOut,Error])
+def update_book(
+    book_id: int,
+    book: BookIn,
+    repo: BookRepository = Depends()
+) -> Union[BookOut,Error]:
+    return repo.update(book_id, book)
+
+@router.get("/books/{book_id}", response_model=Union[BookOut, Error])
+def get_one_book(
+    book_id:int,
+    response: Response,
+    repo: BookRepository = Depends(),
+) -> BookOut:
+    book = repo.get_one(book_id)
+    if book is None:
+        response.status_code = 404
     return book
 
-@router.get("/books")
-def get_book():
-    pass
+@router.delete("/books/{book_id}", response_model=bool)
+def delete_book(
+    book_id:int,
+
+    repo: BookRepository = Depends(),
+) -> bool:
+    return repo.delete(book_id)
