fe3de136bca299b806bb6c8d33aeda99a0ca9066
James Yi <jamesyi803@gmail.com>
2023-02-23 19:02:09 -0800

    Working on frontend

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import './App.css';
-import BookList from './BookList';
-
+// import BookList from './MainPage';
+import MainPage from './MainPage';
 
 function App(props) {
   const [books, setBooks] = useState([])
@@ -24,8 +24,9 @@ function App(props) {
     <div>
       <BrowserRouter>
         <Routes>
+          <Route index element={<MainPage />} />
           <Route path="books">
-            <Route path="" element={<BookList books={books} getBooks ={getBooks}/>}/>
+            {/* <Route path="" element={<BookList books={books} getBooks ={getBooks}/>}/> */}
           </Route>
         </Routes>
       </BrowserRouter>
