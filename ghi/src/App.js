41578678c5203023b274959cfda5a749624825b3
your name <your@email.here>
2023-02-26 23:36:37 -0800

    updates

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -40,25 +40,160 @@
 // export default App;
 
 
-import { useState, createRef } from 'react'
+// import { useState, createRef } from 'react'
+// import './App.css'
+
+// let currentLocation = 1
+// function App() {
+//   let [numOfPapers, setNumOfPapers] = useState([
+//     {
+//       frontText: 'Front 1',
+//       backFont: 'back 1'
+//     },
+//     {
+//       frontText: 'Front 2',
+//       backFont: 'back 2'
+//     },
+//     {
+//       frontText: 'Front 3',
+//       backFont: 'back 3'
+//     }
+//   ])
+//   let papers = numOfPapers.map((item, index) => {
+//     return createRef()
+//   })
+//   let maxLocation = numOfPapers.length + 1;
+//   const preButton = createRef()
+//   const nextButton = createRef()
+//   const book = createRef()
+//   const openBook = function() {
+//     book.current.style.transform = "translateX(50%)";
+//     preButton.current.style.transform = "translateX(-180px)";
+//     nextButton.current.style.transform = "translateX(180px)";
+//   }
+//   const closeBook = function (isAtBeginning) {
+//     if(isAtBeginning) {
+//       book.current.style.transform = "translateX(0%)";
+//     } else {
+//       book.current.style.transform = "translateX(100%)";
+//     }
+//     preButton.current.style.transform = "translateX(0px)";
+//     nextButton.current.style.transform = "translateX(0px)";
+//   }
+//   function goNextPage() {
+//     if(currentLocation < maxLocation) {
+//       if (currentLocation === 1) {
+//         openBook()
+//       }
+//       papers[currentLocation - 1].current.classList.add("flipped");
+//       papers[currentLocation - 1].current.style.zIndex = currentLocation;
+//       if (currentLocation === numOfPapers.length) {
+//         closeBook(false);
+//       }
+//       currentLocation++;
+//     }
+//   }
+
+//   function goPrevPage() {
+//     if (currentLocation > 1) {
+//       if (currentLocation === 2) {
+//         closeBook(true);
+//       }
+//       if (currentLocation === numOfPapers.length + 1) {
+//         openBook();
+//       }
+//       papers[currentLocation - 2].current.classList.remove("flipped");
+//       papers[currentLocation - 2].current.style.zIndex = numOfPapers.length - currentLocation + 2;
+//       currentLocation--;
+//     }
+//   }
+//   // 添加页
+//   function addPage() {
+//     const arr = [...numOfPapers]
+//    arr.push({
+//       frontText: `Front ${numOfPapers.length + 1}`,
+//       backFont: `back  ${numOfPapers.length + 1}`
+//     })
+//     papers = arr.map((item, index) => {
+//       return createRef()
+//     })
+//     setNumOfPapers(arr)
+//   }
+//   // 减少页
+//   function decreasePage() {
+//     const arr = [...numOfPapers]
+//     arr.pop()
+//     papers = arr.map((item, index) => {
+//       return createRef()
+//     })
+//     setNumOfPapers(arr)
+//   }
+//   return (
+//     <div className="App">
+//       <div className='setting-container'>
+//         <button onClick={addPage} className="add">Add a Page</button>
+//         <button onClick={decreasePage} className="add">Delete a Page</button>
+//       </div>
+//       <div className='book-container'>
+//         <button id="prev-btn" ref={preButton} onClick={goPrevPage} className="arrow">
+//           <h1>{'<'}</h1>
+
+//           <i className="fas fa-arrow-circle-left"></i>
+//         </button>
+//         <div id="book" className="book" ref={book}>
+//           {numOfPapers.map((item, index) => {
+//             return (
+//               <div style={{zIndex: numOfPapers.length - index}} className="paper" key={index} ref={papers[index]}>
+//                 <div className="front">
+//                   <div id={'f'+ (index + 1)} className="front-content">
+//                     <h1>{item.frontText}</h1>
+//                   </div>
+//                 </div>
+//                 <div className="back">
+//                   <div id={'b'+ (index + 1)} className="back-content">
+//                     <h1>{item.backFont}</h1>
+//                   </div>
+//                 </div>
+//               </div>
+//             )
+//           })}
+//         </div>
+//         <button id="next-btn" ref={nextButton} onClick={goNextPage} className="arrow">
+//           <h1>{'>'}</h1>
+//           <i className="fas fa-arrow-circle-right"></i>
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import { useState, createRef, useEffect } from 'react'
+import {message, Button, Modal, Form, Input} from 'antd'
 import './App.css'
 
 let currentLocation = 1
 function App() {
   let [numOfPapers, setNumOfPapers] = useState([
     {
-      frontText: 'Front 1',
-      backFont: 'back 1'
+      title: 'Front 1',
+      author: 'back 1',
+      createTime: ''
     },
     {
-      frontText: 'Front 2',
-      backFont: 'back 2'
+      title: 'Front 2',
+      author: 'back 2',
+      createTime: ''
     },
     {
-      frontText: 'Front 3',
-      backFont: 'back 3'
+      title: 'Front 3',
+      author: 'back 3',
+      createTime: ''
     }
   ])
+  let [isModalOpen, setIsModelOpen] = useState(false)
+  const form = createRef()
   let papers = numOfPapers.map((item, index) => {
     return createRef()
   })
@@ -92,6 +227,8 @@ function App() {
       }
       currentLocation++;
     }
+    console.log(123)
+
   }
 
   function goPrevPage() {
@@ -102,40 +239,91 @@ function App() {
       if (currentLocation === numOfPapers.length + 1) {
         openBook();
       }
-      papers[currentLocation - 2].current.classList.remove("flipped");
-      papers[currentLocation - 2].current.style.zIndex = numOfPapers.length - currentLocation + 2;
+      if (papers[currentLocation - 2] && papers[currentLocation - 2].current) {
+        papers[currentLocation - 2].current.classList.remove("flipped");
+        papers[currentLocation - 2].current.style.zIndex = numOfPapers.length - currentLocation + 2;
+      }
       currentLocation--;
+
     }
   }
+  useEffect(() => {
+    if (currentLocation > 1) {
+      for (let i = 1; i < currentLocation; i++) {
+        if (papers[i-1] && papers[i-1].current) {
+          papers[i-1].current.style.zIndex = i;
+        }
+      }
+      if (currentLocation === numOfPapers.length) {
+        book.current.style.transform = "translateX(50%)";
+        preButton.current.style.transform = "translateX(-180px)";
+        nextButton.current.style.transform = "translateX(180px)";
+      }
+    }
+  }, [numOfPapers])
   // 添加页
   function addPage() {
-    const arr = [...numOfPapers]
-   arr.push({
-      frontText: `Front ${numOfPapers.length + 1}`,
-      backFont: `back  ${numOfPapers.length + 1}`
-    })
-    papers = arr.map((item, index) => {
-      return createRef()
-    })
-    setNumOfPapers(arr)
+    setIsModelOpen(true)
   }
   // 减少页
   function decreasePage() {
     const arr = [...numOfPapers]
     arr.pop()
+    if (currentLocation === numOfPapers.length + 1) currentLocation--
+    if (currentLocation === numOfPapers.length) {
+      book.current.style.transform = "translateX(100%)";
+      preButton.current.style.transform = "translateX(0px)";
+      nextButton.current.style.transform = "translateX(0px)";
+    }
     papers = arr.map((item, index) => {
       return createRef()
     })
     setNumOfPapers(arr)
+    message.success({
+      content: '删减成功',
+      duration: 1
+    })
+  }
+  const onFinish = (values) => {
+    if (values) {
+      let currentTime = new Date()
+      let year = currentTime.getFullYear()
+      let month = currentTime.getMonth()
+      let day = currentTime.getDay()
+      values.createTime = `${year}-${month}-${day}`
+      const arr = [...numOfPapers]
+      arr.push(values)
+      papers = arr.map((item, index) => {
+        return createRef()
+      })
+      setNumOfPapers(arr)
+      message.success({
+        content: '添加成功',
+        duration: 1
+      })
+    }
+    form.current.resetFields()
+    setIsModelOpen(false)
+  }
+
+  const handleCancel = () => {
+    form.current.resetFields()
+    setIsModelOpen(false)
+  }
+  const onFinishFailed = () => {
+    message.error({
+      content: 'Input cannot be null',
+      duration: 1
+    })
   }
   return (
     <div className="App">
       <div className='setting-container'>
-        <button onClick={addPage} className="add">Add a Page</button>
-        <button onClick={decreasePage} className="add">Delete a Page</button>
+        <Button type='primary' onClick={() => addPage({title: '123', author: '123'})}>Adding</Button>
+        <Button type='primary' onClick={decreasePage}>Deleting</Button>
       </div>
       <div className='book-container'>
-        <button id="prev-btn" ref={preButton} onClick={goPrevPage} className="arrow">
+        <button id="prev-btn" ref={preButton} onClick={goPrevPage}>
           <h1>{'<'}</h1>
 
           <i className="fas fa-arrow-circle-left"></i>
@@ -145,24 +333,72 @@ function App() {
             return (
               <div style={{zIndex: numOfPapers.length - index}} className="paper" key={index} ref={papers[index]}>
                 <div className="front">
-                  <div id={'f'+ (index + 1)} className="front-content">
-                    <h1>{item.frontText}</h1>
-                  </div>
+                  {/*<div id={'f'+ (index + 1)} className="front-content">*/}
+                  {/*  <span>title:{item.title}</span>*/}
+                  {/*  <span>author:{item.author}</span>*/}
+                  {/*  <span>createTime:{item.createTime}</span>*/}
+                  {/*</div>*/}
+                  <div>title:{item.title}</div>
+                  <div>author:{item.author}</div>
+                  <div>createTime:{item.createTime}</div>
                 </div>
                 <div className="back">
                   <div id={'b'+ (index + 1)} className="back-content">
-                    <h1>{item.backFont}</h1>
+                    <h1>{`back${index+1}`}</h1>
                   </div>
                 </div>
               </div>
             )
           })}
         </div>
-        <button id="next-btn" ref={nextButton} onClick={goNextPage} className="arrow">
+        <button id="next-btn" ref={nextButton} onClick={goNextPage}>
           <h1>{'>'}</h1>
           <i className="fas fa-arrow-circle-right"></i>
         </button>
       </div>
+      <Modal title="add book" open={isModalOpen} onCancel={handleCancel} footer={[]}>
+        <Form
+          ref={form}
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="title"
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: 'title不能为空',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="author"
+            name="author"
+            rules={[
+              {
+                required: true,
+                message: 'author不能为空',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   )
 }
--- ghi/src/main.js
+++ ghi/src/main.js
@@ -8,3 +8,21 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+// import {BrowserRouter, Route, Routes} from 'react-router-dom'
+// import BookPageAdd from "./pages/BookPageAdd/BookPageAdd.jsx";
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />}></Route>
+//         <Route path="/bookPageAdd" element={<BookPageAdd />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// )
