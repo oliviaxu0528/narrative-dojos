4c485d78cd5592f5c63273a64837d03a339a7545
unknown <floydngov@gmail.com>
2023-03-02 11:17:25 -0800

    changes

--- ghi/src/BookDetails.js
+++ ghi/src/BookDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-function BookDetail() {
+function BookDetailCover() {
     const { bookId } = useParams();
     const [book, setBook] = useState(null);
 
