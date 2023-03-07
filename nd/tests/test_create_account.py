c4af1ed2e6d0392a806ec2b7576432a08e904e41
James Yi <jamesyi803@gmail.com>
2023-03-06 16:20:05 -0800

    a

--- nd/tests/test_create_account.py
+++ nd/tests/test_create_account.py
@@ -2,8 +2,7 @@ from fastapi.testclient import TestClient
 from main import app
 from queries.accounts import (AccountRepository,
                               AccountIn,
-                              AccountOutWithPassword
-)
+                              AccountOutWithPassword)
 import random
 import string
 
