d77537781f9994a5322f5a3de8794b2dc99023ef
James Yi <jamesyi803@gmail.com>
2023-03-09 16:07:16 -0800

    b

--- ghi/src/MyBooksList.js
+++ ghi/src/MyBooksList.js
@@ -24,6 +24,7 @@ const MyBooksList = (props) => {
             }
         }
         const response = await fetch(bookUrl,fetchConfig);
+        console.log(response)
         const data = await response.json();
         let arr = [];
         let columns = []
